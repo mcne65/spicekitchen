db6383553cbaac18bcf7d369d9227a681a0be1a0
Sirani McNeill <siranimcneill020@gmail.com>
2019-03-02 13:46:08 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const Home = () => (
     <div className="hero">
       <h1 className="title"> Welcome to Spice Kitchen!</h1>
       <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
+        To edit <code> pages/index.js </code> and view the information.
       </p>
 
       <div className="row">
